4d9d6dcb9e199aca00fda6765436d6087669496c
Fridolin Gross <fridoling@hotmail.com>
2019-04-22 18:43:39 +0200

    make consistent with other scripts

--- src/realign_bam.sh
+++ src/realign_bam.sh
@@ -2,7 +2,7 @@
 
 usage()
 {
-  echo "Usage: $0 [ -f REF ] [ -b BAM]"
+  echo "Usage: ${0##*/} bam -r REF"
   exit 2
 }
 
@@ -18,46 +18,67 @@ set_variable()
   fi
 }
 
-unset REF BAM
+BAM=$1
 
-while getopts 'f:b:?h' c
+unset REF
+OPTIND=2
+while getopts 'r:?h' c
 do
   case $c in
-    f) set_variable REF $OPTARG ;;
-    b) set_variable BAM $OPTARG ;;
+    r) set_variable REF $OPTARG ;;
     h|?) usage ;;
   esac
 done
 
+# check whether input file exists
+if [ ! -f $BAM]; then
+   echo "Error: Please provide paths to an existing BAM file." 
+   exit 2
+fi
+
+# check whether reference is provided
+if [ -z $REF ]; then
+   echo "Error: Please provide path to reference genome"
+   usage
+   exit 2
+fi 
+
+# path to sequencing repository
+SRC="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+PROJECT="$(dirname $SRC)"
+
+PICARD=$PROJECT/bin/picard.jar
+GATK=$PROJECT/bin/GenomeAnalysisTK.jar
+
 # add readgroups to bam file using picard
 # TODO: any meaningful info to put there?
 echo "adding readgroups..."
-BAM_RG="${BAM%.*}"_RG.bam
-java -jar ./bin/GenomeAnalysisTK-3.8-0-ge9d806836/picard.jar AddOrReplaceReadGroups \
+#BAM_RG="${BAM%.*}"_RG.bam
+java -jar $PICARD AddOrReplaceReadGroups \
 I=$BAM \
-O=$BAM_RG RGLB=lib RGPL=illumina RGPU=NA RGSM=30
+O=$BAM RGLB=lib RGPL=illumina RGPU=NA RGSM=30
 echo "...done."
 
 # index resulting bam file
 echo "indexing resulting file..."
-samtools index $BAM_RG
+samtools index $BAM
 echo "...done."
 
 # create target intervals:
 echo "creating  target intervals..."
 IV="${BAM%.*}".intervals
-java -jar ./bin/GenomeAnalysisTK-3.8-0-ge9d806836/GenomeAnalysisTK.jar -T RealignerTargetCreator \
+java -jar $GATK -T RealignerTargetCreator \
 -R $REF \
--I $BAM_RG \
+-I $BAM \
 -o $IV
 echo "...done."
 
 # realign around indels
 echo "realigning..."
 BAM_OUT="${BAM%.*}"_realigned.bam
-java -jar ./bin/GenomeAnalysisTK-3.8-0-ge9d806836/GenomeAnalysisTK.jar -T IndelRealigner \
+java -jar $GATK -T IndelRealigner \
 -R $REF \
 -targetIntervals $IV \
--I $BAM_RG \
+-I $BAM \
 -o $BAM_OUT
 echo "...done."
