2bc66b4bb18914c3a91592d1125571e3558cffcf
Fridolin Gross <fridoling@hotmail.com>
2019-04-22 18:42:51 +0200

    remove renaming and add some basic tests

--- src/prepare_fasta.sh
+++ src/prepare_fasta.sh
@@ -2,7 +2,7 @@
 
 usage()
 {
-  echo "Usage: ${0##*/} fasta1 fasta2 [-d DATE] [-o OUTDIR]"
+  echo "Usage: ${0##*/} fasta1 fasta2 [-o OUTDIR]"
   exit 2
 }
 
@@ -27,10 +27,9 @@ set_variable()
 
 unset DATE OUTDIR
 OPTIND=3
-while getopts 'd:o:?h' c
+while getopts 'o:?h' c
 do
   case $c in
-    d) set_variable DATE $OPTARG ;; 	
     o) set_variable OUTDIR $OPTARG ;;
     h|?) usage ;;
   esac
@@ -57,7 +56,6 @@ SRC="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 PROJECT="$(dirname $SRC)"
 
 # path to used programs
-FASTQC=$PROJECT/bin/FastQC/fastqc
 TRIMMO=$PROJECT/bin/Trimmomatic-0.36/trimmomatic-0.36.jar
 
 # path to adapter sequences
@@ -67,22 +65,11 @@ ADAPTERS=$PROJECT/bin/Trimmomatic-0.36/adapters/TruSeq3-PE-2.fa
 RAWFASTAPATH=$(dirname "${FASTA1}")
 F1NAME=${FASTA1##*/}
 F2NAME=${FASTA2##*/}
-SAMPLE=${F1NAME%%_*}
-
-if [ -z "$DATE" ]; then
-  NEWNAME="$SAMPLE"
-else
-  NEWNAME="$SAMPLE"_"$DATE"
-fi
-
-# get quality info for unprocessed sequences
-#$FASTQC $FASTA1 $FASTA2 --outdir=$RAWFASTAPATH
+BASENAME=${F1NAME%%.*}
 
 # trim fasta files
-java -jar $TRIMMO PE -threads 2 -phred33 -trimlog $OUTDIR"/$NEWNAME".trim_log.txt \
+java -jar $TRIMMO PE -threads 2 -phred33 -trimlog $OUTDIR"/$BASENAME".trim_log.txt \
    $FASTA1 $FASTA2 \
-   -baseout $OUTDIR"/$NEWNAME".trim.fq.gz  \
+   -baseout $OUTDIR"/$BASENAME".trim.fq.gz  \
    ILLUMINACLIP:$ADAPTERS:2:30:8:2:TRUE SLIDINGWINDOW:5:20
 
-# get quality info for processed sequences
-#$FASTQC $OUTDIR"/$NEWNAME".trim_1P.fq.gz $OUTDIR"/$NEWNAME".trim_2P.fq.gz --outdir=$OUTDIR
