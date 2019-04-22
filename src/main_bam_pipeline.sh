54b6fb75498dbbafba19c534497d1e23672fff17
Fridolin Gross <fridoling@hotmail.com>
2019-04-22 18:44:07 +0200

    create first version of pipeline file

---
+++ src/main_bam_pipeline.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+EXPFOLDER=~/Desktop/arbeit/IFOM/DataTest
+DATE=20192104
+SRC=~/Desktop/arbeit/IFOM/sequencing/src
+BIN=~/Desktop/arbeit/IFOM/sequencing/bin
+REF=~/Desktop/arbeit/IFOM/sequencing_nongit/data/ref_genomes/Saccharomyces_cerevisiae.R64-1-1.dna.toplevel.fa
+
+FASTAFOLDER=$EXPFOLDER/rawdata/
+TEMP=$EXPFOLDER/processed
+BAMFOLDER=$EXPFOLDER/bamfiles
+
+ls $FASTAFOLDER*R1_001.fastq.gz | sed 's/_R1_001.*//' |\
+(while read FILENAME; do
+   F1="$FILENAME"_R1_001.fastq.gz 
+   F2="$FILENAME"_R2_001.fastq.gz 
+
+   # get quality info for unprocessed sequences
+   $BIN/FastQC/fastqc $F1 $F2 --outdir=$FASTAFOLDER
+
+   # trim fasta sequences
+   $SRC/prepare_fasta.sh $F1 $F2 -o $FASTAFOLDER
+done)
+
+ls $FASTAFOLDER*.trim_1P.fq.gz | sed 's/.trim_1P.fq.gz//' |\
+(while read FILENAME; do
+   F1="$FILENAME".trim_1P.fq.gz
+   F2="$FILENAME".trim_2P.fq.gz
+   $SRC/fasta2bam.sh $F1 $F2 -r $REF -o $BAMFOLDER
+done)
+
+ls $BAMFOLDER/*.bam |
+(while read FILENAME; do
+  $SRC/realign_bam.sh $FILENAME -r $REF
+done)
