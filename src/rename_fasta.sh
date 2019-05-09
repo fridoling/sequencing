3f290675e96f0af659d7248fe32853ed3c271c5e
pilopilota <gabonzo@gmail.com>
2019-05-09 09:26:20 +0200

    Add script to rename fasta files

---
+++ src/rename_fasta.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+usage()
+{
+  echo "Usage: ${0##*/} inputdir prefix "
+  exit 2
+}
+
+if [ $# -lt 2 ]; then
+    usage
+fi
+
+INPUTDIR=$1
+PREFIX=$2
+
+set_variable()
+{
+  local varname=$1
+  shift
+  if [ -z "${!varname}" ]; then
+    eval "$varname=\"$@\""
+  else
+    echo "Error: $varname already set"
+    usage
+  fi
+}
+
+# check whether input directory exist
+if [ ! -d $INPUTDIR ]; then
+   echo "Error: Please provide paths to existing folder." 
+   exit 2
+fi
+
+
+
+# rename files in INPUTDIR
+for file in $INPUTDIR/*.gz
+do
+    filename=$(basename "$file")
+    # check if file name already starts with PREFIX 
+    if [[ $filename != "$PREFIX"* ]]; then
+	newfilename="$PREFIX"_$filename
+	newfile=$INPUTDIR/$newfilename
+	# check if the new filename is already taken
+	if [ -f $newfile ]; then
+	    echo Skip $filename: $newfilename already exists
+	else
+	    echo mv $file $newfile
+	fi
+    fi
+done
+
