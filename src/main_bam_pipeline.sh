6b7a741e923e3fab555de435a9081387ce6dae76
pilopilota <gabonzo@gmail.com>
2019-05-14 17:16:15 +0200

    Uncomment what's before bam realignment, and run quality check on realignemd bams

--- src/main_bam_pipeline.sh
+++ src/main_bam_pipeline.sh
@@ -22,44 +22,45 @@ fi
 {
     echo -e "START EVERYTHING AT " $(date) "\n\n"
 
-#    # rename fasta files using the date of experiment
-#    echo -e "*** \t \t Step 1: rename original fasta files  \t \t ***"
-#    $SRC/rename_fasta.sh $FASTAFOLDER $DATE
-#    echo -e "*** RENAMING: done \n\n\n"
-#
-#    ls $FASTAFOLDER*R1_001.fastq.gz | sed 's/_R1_001.*//' |\
-#	(while read FILENAME; do
-#	     F1="$FILENAME"_R1_001.fastq.gz 
-#	     F2="$FILENAME"_R2_001.fastq.gz 
-#	     
-#	     echo -e "*** trim $FILENAME"
-#
-#	     # get quality info for unprocessed sequences
-#	     $BIN/FastQC/fastqc $F1 $F2 --outdir=$LOGFOLDER
-#	     
-#	     # trim fasta sequences
-#	     $SRC/prepare_fasta.sh $F1 $F2 -o $FASTAFOLDER
-#	 done)
-#   echo -e "*** TRIMMING: done \n\n\n"
-#  
-#    ls $FASTAFOLDER*.trim_1P.fq.gz | sed 's/.trim_1P.fq.gz//' |\
-#	(while read FILENAME; do
-#	     F1="$FILENAME".trim_1P.fq.gz
-#	     F2="$FILENAME".trim_2P.fq.gz
-#
-#	     echo -e "*** prepare bam from $FILENAME"
-#
-#	     $SRC/fasta2bam.sh $F1 $F2 -r $REF -o $BAMFOLDER
-#	 done)
-#   echo -e "*** BAM CREATION: done \n\n\n"
+    # rename fasta files using the date of experiment
+    echo -e "*** \t \t Step 1: rename original fasta files  \t \t ***"
+    $SRC/rename_fasta.sh $FASTAFOLDER $DATE
+    echo -e "*** RENAMING: done \n\n\n"
+
+    ls $FASTAFOLDER*R1_001.fastq.gz | sed 's/_R1_001.*//' |\
+	(while read FILENAME; do
+	     F1="$FILENAME"_R1_001.fastq.gz 
+	     F2="$FILENAME"_R2_001.fastq.gz 
+
+	     echo -e "*** trim $FILENAME"
+
+	     # get quality info for unprocessed sequences
+	     $BIN/FastQC/fastqc $F1 $F2 --outdir=$LOGFOLDER
+	     
+	     # trim fasta sequences
+	     $SRC/prepare_fasta.sh $F1 $F2 -o $FASTAFOLDER
+	 done)
+   echo -e "*** TRIMMING: done \n\n\n"
+  
+    ls $FASTAFOLDER*.trim_1P.fq.gz | sed 's/.trim_1P.fq.gz//' |\
+	(while read FILENAME; do
+	     F1="$FILENAME".trim_1P.fq.gz
+	     F2="$FILENAME".trim_2P.fq.gz
+
+	     echo -e "*** prepare bam from $FILENAME"
+
+	     $SRC/fasta2bam.sh $F1 $F2 -r $REF -o $BAMFOLDER
+	 done)
+   echo -e "*** BAM CREATION: done \n\n\n"
     
     ls $BAMFOLDER/*.bam |
 	(while read FILENAME; do
 	     echo -e "*** realign $FILENAME"
 	     $SRC/realign_bam.sh $FILENAME -r $REF
 
-	     echo -e "*** get information about $FILENAME quality"
-	     $BIN/qualimap_v2.2.1/qualimap bamqc -bam $FILENAME -nw 400 -hm 3
+	     REALIGNEDFILE= "${FILENAME%%.*}"_realigned.bam
+	     echo -e "*** get information about $REALIGNEDFILE quality"
+	     $BIN/qualimap_v2.2.1/qualimap bamqc -bam $REALIGNEDFILE -nw 400 -hm 3
 
 	 done)
     echo -e "*** BAM REALIGNMENT and QUALITY CHECK: done \n\n\n"
