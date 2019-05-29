94a9ce29925743c2d3b584131260123cc52ac425
pilopilota <gabonzo@gmail.com>
2019-05-29 13:26:05 +0200

    for loop on samples

--- src/main_bam_pipeline.sh
+++ src/main_bam_pipeline.sh
@@ -28,13 +28,15 @@ fi
 #    echo -e "*** RENAMING: done \n\n\n"
 
     ls $FASTAFOLDER*_1.fq.gz | sed 's/_1.fq.gz//' |\
-	(while read FILENAME; do
-	     F1="$FILENAME"_1.fq.gz 
-	     F2="$FILENAME"_2.fq.gz
+	(while read SAMPLENAME; do
 
+	     echo -e "\n\n\n*** ANALYZE  $SAMPLENAME"
+	     
 	     # check gz file integrity
+	     F1="$SAMPLENAME"_1.fq.gz 
+	     F2="$SAMPLENAME"_2.fq.gz
 	     if  gzip -t $F1  &&  gzip -t $F2  ; then
-		 echo -e "\n\n*** trim $FILENAME"
+		 echo -e "*** trim $SAMPLENAME"
 		 # get quality info for unprocessed sequences
 		 $BIN/FastQC/fastqc $F1 $F2 --outdir=$LOGFOLDER
 	     
@@ -42,35 +44,27 @@ fi
 		 $SRC/prepare_fasta.sh $F1 $F2 -o $FASTAFOLDER
 	     else
 		 echo -e "\n\nWARNING: $F1 or $F2 corrupted "
+		 continue
 	     fi
-	 done)
-    echo -e "*** TRIMMING: done \n\n\n"
+	     echo -e "*** done \n"
 
-    
-    ls $FASTAFOLDER*.trim_1P.fq.gz | sed 's/.trim_1P.fq.gz//' |\
-	(while read FILENAME; do
-	     F1="$FILENAME".trim_1P.fq.gz
-	     F2="$FILENAME".trim_2P.fq.gz
-
-	     echo -e "*** prepare bam from $FILENAME"
-
+	     ## PREPARE BAM
+	     F1="$SAMPLENAME".trim_1P.fq.gz
+	     F2="$SAMPLENAME".trim_2P.fq.gz
+	     echo -e "*** prepare bam from $SAMPLENAME"
 	     $SRC/fasta2bam.sh $F1 $F2 -r $REF -o $BAMFOLDER
-	 done)
-    echo -e "*** BAM CREATION: done \n\n\n"
+	     echo -e "*** done \n"
 
+	     ## REALIGN BAM
+	     echo -e "*** realign $SAMPLENAME"
+	     $SRC/realign_bam.sh "$SAMPLENAME".bam -r $REF
+	     echo -e "*** done \n"
 
-    
-    ls $BAMFOLDER/*.bam |
-	(while read FILENAME; do
-	     echo -e "*** realign $FILENAME"
-	     $SRC/realign_bam.sh $FILENAME -r $REF
-
-	     REALIGNEDFILE="${FILENAME%%.*}"_realigned.bam
+	     REALIGNEDFILE="${SAMPLENAME%%.*}"_realigned.bam
 	     echo -e "*** get information about $REALIGNEDFILE quality"
 	     $BIN/qualimap_v2.2.1/qualimap bamqc -bam $REALIGNEDFILE -nw 400 -hm 3
-
-	 done)
-    echo -e "*** BAM REALIGNMENT and QUALITY CHECK: done \n\n\n"
+	     echo -e "*** done \n\n\n"
+	 done
+	 )
     echo -e "EVERYTHING DONE AT " $(date) "\n\n"
-
 } 2>&1 | tee $LOGFILE
