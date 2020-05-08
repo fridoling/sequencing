03b077d9ebe808093295f37964c2af2ef9e17c97
pilopilota <gabonzo@gmail.com>
2020-05-08 15:11:59 +0200

    restore evoGA file

--- src/main_bam_pipeline.sh
+++ src/main_bam_pipeline.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
-EXPFOLDER=/lustre/data/ANC/NGS/20181106_evo_facility_GA12/
-DATE=20181106
+EXPFOLDER=/lustre/data/ANC/NGS/20190516_GA12/
+DATE=20190516
 
+EXPFOLDER=/lustre/data/ANC/NGS/20190929_GA5/
+DATE=20190929
 
 SRC=/lustre/data/ANC/NGS/sequencing/src
 BIN=/lustre/data/ANC/NGS/sequencing/bin
 REF=/lustre/data/ANC/NGS/ref/Saccharomyces_cerevisiae.R64-1-1.dna.toplevel.fa
 
-SAMPLELIST=(a1 b1 b2 c2 a4 b4 c4 d4 e4 f4 b5 a7 b7 c7 d7 h7 a8 c8 a10 b10 a11 b11 a12 b12)
-SAMPLELIST=( a1 )
+SAMPLELIST=(A1 B1 C1 D1 E1 F1 G1 H1 A2 B2 C2 D2 E2 F2 G2 H2 A4 B4 C4 D4 E4 F4 G4  H4   A5   B5 C5  D5 E5 F5 G5 H5  A6   B6   C6   D6   E6  F6 G6 H6 A7   B7   C7   D7   E7   F7   G7   H7   A8   B8   C8   D8   E8   F8   G8   H8   A9   B9   C9   D9   E9   F9   G9   H9  A10  B10  C10  D10 E10  F10  G10  H10  A11  B11  C11  D11  E11  F11  G11  H11  A12  B12  C12  D12  E12  F12  G12  H12)
 
 FASTAFOLDER=$EXPFOLDER/rawdata/
 TEMP=$EXPFOLDER/processed
@@ -24,9 +25,6 @@ if [ ! -d $LOGFOLDER ]; then
    mkdir -m 775 $LOGFOLDER
 fi
 
-# suffixes
-suffF1="_R1_001.fastq.gz"
-suffF2="_R2_001.fastq.gz"
 {
     echo -e "START EVERYTHING AT " $(date) "\n\n"
 
@@ -35,16 +33,11 @@ suffF2="_R2_001.fastq.gz"
 #    $SRC/rename_fasta.sh $FASTAFOLDER $DATE
 #    echo -e "*** RENAMING: done \n\n\n"
 
-    ls  ${FASTAFOLDER}*${suffF1} | sed 's/'${suffF1}'//' |\
+    ls $FASTAFOLDER*_1.fq.gz | sed 's/_1.fq.gz//' |\
 	(while read SAMPLENAME; do
 	     # CHECK if SAMPLE is one of the SAMPLELIST
 	     SAMPLE=$(basename "$SAMPLENAME")
-	     # remove 20190929_ as prefix SAMPLE
-	     #SAMPLE="${SAMPLE/20190929_/}"
-	     # remove suffix from SAMPLE
-	     IFS='_' tokens=( $SAMPLE )
-	     SAMPLE=${tokens[0]}
-	     
+	     SAMPLE="${SAMPLE/20190929_/}"
 	     isthere=0
 	     for i in "${SAMPLELIST[@]}"
 	     do
@@ -55,19 +48,12 @@ suffF2="_R2_001.fastq.gz"
 	     # if the sample is one of the list, run
 	     if [ $isthere == 1 ] ; then 
 		 
-		 echo -e "\n\n\n*** ANALYZE  ${SAMPLENAME}${suffF1}"
+		 echo -e "\n\n\n*** ANALYZE  $SAMPLENAME"
 	     
 		 # check gz file integrity
-		 F1="${SAMPLENAME}${suffF1}" 
-		 F2="${SAMPLENAME}${suffF2}"
-
-		 #echo prova
-		 #echo -e "${SAMPLENAME}"
-		 #echo -e $F1
-		 #echo -e ${F1}
-		 #echo -e "${F1}"
-		 echo gzip -t "${F1}"  &&  gzip -t "${F2}"
-		 if  gzip -t "${F1}"  &&  gzip -t "${F2}"  ; then
+		 F1="$SAMPLENAME"_1.fq.gz 
+		 F2="$SAMPLENAME"_2.fq.gz
+		 if  gzip -t $F1  &&  gzip -t $F2  ; then
 		     echo -e "*** trim $SAMPLENAME"
 		     # get quality info for unprocessed sequences
 		     $BIN/FastQC/fastqc $F1 $F2 --outdir=$LOGFOLDER
@@ -98,8 +84,6 @@ suffF2="_R2_001.fastq.gz"
 		 echo -e "*** get information about $REALIGNEDFILE quality"
 		 $BIN/qualimap_v2.2.1/qualimap bamqc -bam $REALIGNEDFILE -nw 400 -hm 3
 		 echo -e "*** done \n\n\n"
-	     else
-		 echo -e "\n*** $SAMPLE not in the list"
 	     fi
 	 done
 	 )
