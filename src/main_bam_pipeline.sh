5612bf812b63b5441b0464697dd806aa5d56ffa7
pilopilota <gabonzo@gmail.com>
2020-05-08 14:21:30 +0200

    debug

--- src/main_bam_pipeline.sh
+++ src/main_bam_pipeline.sh
@@ -57,8 +57,8 @@ suffF2="_R2_001.fastq.gz"
 		 echo -e "\n\n\n*** ANALYZE  ${SAMPLENAME}${suffF1}"
 	     
 		 # check gz file integrity
-		 F1="$SAMPLENAME$suffF1" 
-		 F2="$SAMPLENAME$suffF2"
+		 F1="a ${SAMPLENAME}${suffF1}" 
+		 F2="a ${SAMPLENAME}${suffF2}"
 
 		 echo -e F1 $F1 
 		 echo -e F2 $F2 
